8bf9436c30b794a3ad5b79ffb7f9199da7a105a3
shoob <162135034+shoob17@users.noreply.github.com>
2025-03-28 08:50:49 -0400

    Added map and autosave
    Set up a map to set up player movement

--- Dennis_Files/Assets/Scripts/RoomManager.cs
+++ Dennis_Files/Assets/Scripts/RoomManager.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class RoomManager : MonoBehaviour
+public class RoomManager : MonoBehaviourPunCallbacks
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public string roomCode = "Map1";
+
     void Start()
     {
-        
+        // 1 - Connect to master server (this is like a main menu with settings, store, room list, etc.)
+        // 2 - Join the lobby (view all the active games/servers "rooms" in Photon)
+        // 3 - Join one of the rooms
+        // 4 - Spawn the player on one of the rooms/servers
+
+        Debug.Log("Connecting...");
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        Debug.Log("Joining lobby...");
+
+        PhotonNetwork.JoinLobby();
     }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joining or creating room...");
+
+        PhotonNetwork.JoinOrCreateRoom(roomCode, null, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room. Spawning player!");
+
+
+    }
+
+
+
 }
